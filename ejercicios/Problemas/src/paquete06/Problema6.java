fd7c59fa4e887110a55c493b7feb664e116e40ce
mesolano9 <mariaemiliasolano43@gmail.com>
2026-01-09 12:04:45 -0500

    Problema5

--- ejercicios/Problemas/src/paquete06/Problema6.java
+++ ejercicios/Problemas/src/paquete06/Problema6.java
@@ -5,10 +5,43 @@
  */
 package paquete06;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+        double media = calcularMedia(informacion);
+        double desviacion = calcularDesviacion(informacion, media);
+        
+        System.out.printf("%.2f\n%.2f\n",media,desviacion);
+    }
+    public static double calcularMedia(int[] arreglo){
+        double suma = 0;
+        double variable;
+        for(int i = 0; i < arreglo.length; i++){
+            suma =  suma + arreglo[i];   
+        }    
+        variable = suma/arreglo.length;
+        return variable;
+    }
+    public static double calcularDesviacion(int[] arreglo, double media){
+        double suma = 0;
+        double variable;
+        for(int i = 0; i < arreglo.length; i++){
+            suma = suma + Math.pow(arreglo[i] - media, 2);
+        }  
+        variable = Math.sqrt(suma/arreglo.length);
+        return variable;
+    }   
 }
+
+
--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -5,10 +5,55 @@
  */
 package paquete07;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String[] mensajeCiudadesEcuador = ciudades();
+
+        System.out.println("Ciudades ingresadas:");
+        for (int i = 0; i < mensajeCiudadesEcuador.length; i++) {
+            System.out.println(mensajeCiudadesEcuador[i]);
+        }
+        System.out.println("Ciudades con 4 o 5 caracteres:");
+
+        for (int i = 0; i < mensajeCiudadesEcuador.length; i++) {
+            if (mensajeCiudadesEcuador[i].length() == 4 || mensajeCiudadesEcuador[i].length() == 5) {
+                System.out.println(mensajeCiudadesEcuador[i]);
+            }
+        }
+    }
+
+    public static String[] ciudades() {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        int numeroCiudades;
+        String nombreCiudades;
+
+        System.out.println("Ingrese el número de ciudades:");
+        numeroCiudades = entrada.nextInt();
+
+        entrada.nextLine();
+
+        String[] ciudadesEcuador = new String[numeroCiudades];
+        for (int i = 0; i < numeroCiudades; i++) {
+            System.out.printf("Ingrese el nombre de la ciudad número %d:"
+                    + " ", i + 1);
+
+            nombreCiudades = entrada.nextLine();
+
+            ciudadesEcuador[i] = nombreCiudades;
+        }
+        return ciudadesEcuador;
+    }
 }
