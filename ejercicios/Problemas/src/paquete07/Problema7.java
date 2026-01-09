413b0b5561ef223f0f9a421724582121ce5deedf
mesolano9 <mariaemiliasolano43@gmail.com>
2026-01-09 12:15:49 -0500

    Problema7

--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -19,41 +19,43 @@ public class Problema7 {
         entrada.useLocale(Locale.US);
 
         String[] mensajeCiudadesEcuador = ciudades();
-
+        
         System.out.println("Ciudades ingresadas:");
-        for (int i = 0; i < mensajeCiudadesEcuador.length; i++) {
+        for(int i = 0; i < mensajeCiudadesEcuador.length; i++) {
             System.out.println(mensajeCiudadesEcuador[i]);
         }
         System.out.println("Ciudades con 4 o 5 caracteres:");
-
-        for (int i = 0; i < mensajeCiudadesEcuador.length; i++) {
-            if (mensajeCiudadesEcuador[i].length() == 4 || mensajeCiudadesEcuador[i].length() == 5) {
+        
+        for(int i = 0; i < mensajeCiudadesEcuador.length; i++) {
+            if (mensajeCiudadesEcuador[i].length() == 4 || mensajeCiudadesEcuador[i].length() == 5) 
+            {
                 System.out.println(mensajeCiudadesEcuador[i]);
             }
         }
     }
-
+    
     public static String[] ciudades() {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-
+        
         int numeroCiudades;
         String nombreCiudades;
-
+        
         System.out.println("Ingrese el número de ciudades:");
         numeroCiudades = entrada.nextInt();
-
+        
         entrada.nextLine();
-
+        
         String[] ciudadesEcuador = new String[numeroCiudades];
         for (int i = 0; i < numeroCiudades; i++) {
             System.out.printf("Ingrese el nombre de la ciudad número %d:"
                     + " ", i + 1);
-
+            
             nombreCiudades = entrada.nextLine();
-
+            
             ciudadesEcuador[i] = nombreCiudades;
         }
         return ciudadesEcuador;
     }
 }
+
